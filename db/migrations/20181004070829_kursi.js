04aed366987080fa4c0ffcb2250763bda080334b
danilrafiqi <danil.rafiqi@gmail.com>
2018-10-05 11:02:31 +0700

    model yang baru fix

--- db/migrations/20181004070829_kursi.js
+++ db/migrations/20181004070829_kursi.js
@@ -3,8 +3,8 @@ exports.up = function(knex, Promise) {
 	return knex.schema.createTable('kursi', function (table) {
 		table.uuid('id').primary().unique()
 		table.string('kode');
-		table.uuid('id_kursi');
-		table.foreign('id_kursi').references('id').inTable('kursi');
+		table.uuid('id_bus');
+		table.foreign('id_bus').references('id').inTable('bus');
 		table.timestamp('created_at').defaultTo(knex.fn.now());
 		table.timestamp('updated_at').defaultTo(knex.fn.now());
 	})
