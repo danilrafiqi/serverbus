b1bf59ddade34ac7c6711885e566cf9fbc31c92b
Muhamad Danil Rafiqi <danil.rafiqi@gmail.com>
2018-11-21 17:01:34 +0700

    po ctrl

--- controllers/poCtrl.js
+++ controllers/poCtrl.js
@@ -1,75 +1,76 @@
-const knex = require('../db/knex')
-const table = 'po'
-const knexDate= knex.fn.now()
-const uuidv4 = require('uuid/v4')
-
-
+const knex = require('../db/knex');
+const table = 'po';
+const knexDate = knex.fn.now();
+const uuidv4 = require('uuid/v4');
 
 module.exports = {
-	all: (req, res) => {
-		knex.select().from(table)
-	    .then(datas =>{
-	      res.send(datas)
-	    })
-	},
+  all: (req, res) => {
+    knex
+      .select()
+      .from(table)
+      .then(datas => {
+        res.send(datas);
+      });
+  },
 
-	detail: (req, res) => {
-		knex.select().from(table)
-			.where('id_po', req.params.id)
-	    .then(datas =>{
-	      res.send(datas)
-	    })
-	},
+  detail: (req, res) => {
+    knex
+      .select()
+      .from(table)
+      .where('id_po', req.params.id)
+      .then(datas => {
+        res.send(datas);
+      });
+  },
 
-	post: (req, res) => {
-		const data = {
-			id_po: uuidv4(),
-			nama: req.body.nama,
-			alamat: req.body.alamat,
-			no_hp: req.body.no_hp,
-			email: req.body.email
-		}
+  post: (req, res) => {
+    const data = {
+      id_po: uuidv4(),
+      nama: req.body.nama,
+      alamat: req.body.alamat,
+      no_hp: req.body.no_hp,
+      email: req.body.email
+    };
 
-		knex(table).insert(data)
-	    	.then(datas =>{
-	     		res.send('success')
-	   		})
-	    	.catch(err =>{
-	      		res.send('error disini : '+ err)
-	    	})
-	},
+    knex(table)
+      .insert(data)
+      .then(datas => {
+        res.send('success');
+      })
+      .catch(err => {
+        res.send('error disini : ' + err);
+      });
+  },
 
-	edit:(req, res) => {
-		
-		const data = {
-			nama: req.body.nama,
-			alamat: req.body.alamat,
-			no_hp: req.body.no_hp,
-			email: req.body.email,
-			updated_at: knexDate,
-			updated_at: knexDate
-		}
-		knex(table)
-		    .where('id_po', req.params.id)
-		    .update(data)
-		    .then(datas =>{
-		      res.send('success update : '+ req.params.id)
-		    })
-		    .catch(err =>{
-		      res.send('error disini : '+ err)
-		    })
-	},
+  edit: (req, res) => {
+    const data = {
+      nama: req.body.nama,
+      alamat: req.body.alamat,
+      no_hp: req.body.no_hp,
+      email: req.body.email,
+      updated_at: knexDate,
+      updated_at: knexDate
+    };
+    knex(table)
+      .where('id_po', req.params.id)
+      .update(data)
+      .then(datas => {
+        res.send('success update : ' + req.params.id);
+      })
+      .catch(err => {
+        res.send('error disini : ' + err);
+      });
+  },
 
-	delete: (req, res) => {
-	    knex(table)
-	    .where('id_po', req.params.id)
-	    .del()
-	    .then(() =>{
-	      res.send('success delete : '+ req.params.id)
-	    })
-	    .catch(err =>{
-	      res.send('error disini : '+ err)
-	    })
-	}
-
-}
+  delete: (req, res) => {
+    knex(table)
+      .where('id_po', req.params.id)
+      .del()
+      .then(() => {
+        res.send('success delete : ' + req.params.id);
+      })
+      .catch(err => {
+        res.send('error disini : ' + err);
+      });
+  }
+};
--- server.js
+++ server.js
@@ -1,50 +1,49 @@
-const express = require('express')
-const app = express()
-const bodyParser = require("body-parser");
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-
-const po = require('./routes/po')
-const rute = require('./routes/rute')
-const kelas = require('./routes/kelas')
-const penumpang = require('./routes/penumpang')
-const aturan = require('./routes/aturan')
-const admin = require('./routes/admin')
-const tujuan = require('./routes/tujuan')
-const pemesan = require('./routes/pemesan')
-const bank = require('./routes/bank')
-const agen = require('./routes/agen')
-const user = require('./routes/user')
-const bus = require('./routes/bus')
+const po = require('./routes/po');
+const rute = require('./routes/rute');
+const kelas = require('./routes/kelas');
+const penumpang = require('./routes/penumpang');
+const aturan = require('./routes/aturan');
+const admin = require('./routes/admin');
+const tujuan = require('./routes/tujuan');
+const pemesan = require('./routes/pemesan');
+const bank = require('./routes/bank');
+const agen = require('./routes/agen');
+const user = require('./routes/user');
+const bus = require('./routes/bus');
 //const kursi = require('./routes/kursi')
-const transaksi = require('./routes/transaksi')
-const jadwal = require('./routes/jadwal')
-const harga = require('./routes/harga')
-const tiket = require('./routes/tiket')
+const transaksi = require('./routes/transaksi');
+const jadwal = require('./routes/jadwal');
+const harga = require('./routes/harga');
+const tiket = require('./routes/tiket');
 
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/po', po)
-app.use('/rute', rute)
-app.use('/kelas', kelas)
-app.use('/penumpang', penumpang)
-app.use('/aturan', aturan)
-app.use('/admin', admin)
-app.use('/tujuan', tujuan)
-app.use('/pemesan', pemesan)
-app.use('/bank', bank)
-app.use('/agen', agen)
-app.use('/user', user)
-app.use('/bus', bus)
+app.use('/po', po);
+app.use('/rute', rute);
+app.use('/kelas', kelas);
+app.use('/penumpang', penumpang);
+app.use('/aturan', aturan);
+app.use('/admin', admin);
+app.use('/tujuan', tujuan);
+app.use('/pemesan', pemesan);
+app.use('/bank', bank);
+app.use('/agen', agen);
+app.use('/user', user);
+app.use('/bus', bus);
 //app.use('/kursi', kursi)
-app.use('/transaksi', transaksi)
-app.use('/jadwal', jadwal)
-app.use('/harga', harga)
-app.use('/tiket', tiket)
-
+app.use('/transaksi', transaksi);
+app.use('/jadwal', jadwal);
+app.use('/harga', harga);
+app.use('/tiket', tiket);
 
 const port = process.env.PORT || 2018;
 app.listen(port, process.env.IP, () => {
-  console.log("Server Started on Port: ", port);
+  console.log('Server Started on Port: ', port);
 });
