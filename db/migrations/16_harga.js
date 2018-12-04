036ba0e32dbd8294c6d0f05880a0674361342eb8
Muhamad Danil Rafiqi <danil.rafiqi@gmail.com>
2018-12-04 15:21:27 +0700

    ntah apa yg berubah

--- db/migrations/16_harga.js
+++ db/migrations/16_harga.js
@@ -20,6 +20,8 @@ exports.up = function(knex, Promise) {
       .foreign('id_agen')
       .references('id_agen')
       .inTable('agen');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
   });
 };
 exports.down = function(knex, Promise) {
--- routes/jadwal.js
+++ routes/jadwal.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const jadwalCtrl = require('../controllers/jadwalCtrl')
+const express = require('express');
+const router = express.Router();
+const jadwalCtrl = require('../controllers/jadwalCtrl');
 
-router.get('/',jadwalCtrl.all )
-router.get('/:id',jadwalCtrl.detail )
-router.post('/', jadwalCtrl.post)
-router.put('/:id', jadwalCtrl.edit)
-router.delete('/:id', jadwalCtrl.delete)
+router.get('/', jadwalCtrl.all);
+router.get('/:id_jadwal', jadwalCtrl.detail);
+router.post('/', jadwalCtrl.post);
+router.put('/:id_jadwal', jadwalCtrl.edit);
+router.delete('/:id_jadwal', jadwalCtrl.delete);
 
 module.exports = router;
