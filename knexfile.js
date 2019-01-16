3e193610d9ac150a1beb342e07f35666056a3a91
Muhamad Danil Rafiqi <danil.rafiqi@gmail.com>
2019-01-16 15:03:53 +0700

    add sqlite3

--- knexfile.js
+++ knexfile.js
@@ -16,14 +16,11 @@ module.exports = {
       directory: __dirname + '/db/seeds'
     }
   },
+
   production: {
-    client: 'pg',
+    client: 'sqlite3',
     connection: {
-      host: 'ec2-107-20-183-142.compute-1.amazonaws.com',
-      user: 'uhtqedovcbzcqb',
-      password:
-        '1a6ff2968601937c83a65a9930e8e617bbb2e180a47c47c39574bcfde913c6d3',
-      database: 'des1v8279t82km'
+      filename: __dirname + '/db/db_busnew.sqlite'
     },
     migrations: {
       directory: __dirname + '/db/migrations'
@@ -32,4 +29,21 @@ module.exports = {
       directory: __dirname + '/db/seeds'
     }
   }
+
+  // production: {
+  //   client: 'pg',
+  //   connection: {
+  //     host: 'ec2-107-20-183-142.compute-1.amazonaws.com',
+  //     user: 'uhtqedovcbzcqb',
+  //     password:
+  //       '1a6ff2968601937c83a65a9930e8e617bbb2e180a47c47c39574bcfde913c6d3',
+  //     database: 'des1v8279t82km'
+  //   },
+  //   migrations: {
+  //     directory: __dirname + '/db/migrations'
+  //   },
+  //   seeds: {
+  //     directory: __dirname + '/db/seeds'
+  //   }
+  // }
 };
