7fc73e2fee6841f3db7e9eb039bba349674a3304
Muhamad Danil Rafiqi <danil.rafiqi@gmail.com>
2019-01-18 14:24:57 +0700

    change query pencarian

--- controllers/cariCtrl.js
+++ controllers/cariCtrl.js
@@ -14,21 +14,36 @@ module.exports = {
       });
   },
   jadwal: (req, res) => {
+    // knex
+    //   .select()
+    //   .from('carijadwal')
+    //   .where({
+    //     pemberangkatan: 'Bandar Lampung',
+    //     pemberhentian: 'Kebumen'
+    //   })
+    //   .andWhere(function() {
+    //     this.where('tanggal_keberangkatan', 'like', `2019-01-20%`);
+    //   })
+    //   //tambahin where biar tidak bisa serch jadwal yang udah lewat tanggalnya atau di ubah di form input tanggal keberangkatan
+    //   .then(datas => {
+    //     console.log('brgkt', req.query.pemberangkatan);
+    //     console.log('brnti', req.query.pemberhentian);
+    //     console.log('tgl', req.query.tanggal_keberangkatan);
+    //     console.log('daa', datas);
+    //     res.send(datas);
+    //   });
+
+    // query with raw query
     knex
-      .select()
-      .from('carijadwal')
-      .where({
-        pemberangkatan: req.query.pemberangkatan,
-        pemberhentian: req.query.pemberhentian
-      })
-      .andWhere(function() {
-        this.where(
-          'tanggal_keberangkatan',
-          'like',
-          `${req.query.tanggal_keberangkatan}%`
-        );
-      })
-      //tambahin where biar tidak bisa serch jadwal yang udah lewat tanggalnya atau di ubah di form input tanggal keberangkatan
+      .raw(
+        `
+    SELECT * FROM carijadwal WHERE tanggal_keberangkatan like '${
+      req.query.tanggal_keberangkatan
+    }%' and pemberangkatan like '${
+          req.query.pemberangkatan
+        }' and pemberhentian like '${req.query.pemberhentian}'
+    `
+      )
       .then(datas => {
         res.send(datas);
       });
