010c1749fe94df26689f45c455c2f880cfb9f2c2
Muhamad Danil Rafiqi <danil.rafiqi@gmail.com>
2019-01-21 14:34:54 +0700

    fix

--- db/seeds/13_operator.js
+++ db/seeds/13_operator.js
@@ -9,7 +9,7 @@ exports.seed = function(knex, Promise) {
       return knex('operator').insert([
         {
           id: '255f4c32-5678-1234-8cf2-f75c32fcf158',
-          email: 'gede@gmail.com',
+          email: 'gedearya359@gmail.com',
           nama: 'I Gede Arya',
           jenis_kelamin: 'l',
           foto: 'default.jpg',
@@ -17,14 +17,14 @@ exports.seed = function(knex, Promise) {
         },
         {
           id: '255f4c32-5678-4321-8cf2-f75c32fcf158',
-          email: 'deni@gmail.com',
+          email: 'denysaputra1333@gmail.com',
           nama: 'Deni Saputra',
           jenis_kelamin: 'l',
           foto: 'default.jpg',
           po_id: '3ab51aac-f534-47e8-8184-f516077df3be'
         },
         {
-          id: '255f4c32-5678-4321-8cf2-f75c32fcf158',
+          id: '255f4c32-5678-4321-8cf2-f75c32fcf159',
           email: 'petugas@gmail.com',
           nama: 'Petugas',
           jenis_kelamin: 'l',
